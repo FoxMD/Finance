472e5b8226a124376b566c5427419e018aaf30dd
FoxMD <ryusei@seznam.cz>
2020-11-08 10:41:29 +0100

    Reader

--- files/filemanager.py
+++ files/filemanager.py
@@ -1,5 +1,5 @@
 import os
-
+import csv
 
 class Filemanager:
     root_dir = "./database/"
@@ -14,9 +14,26 @@ class Filemanager:
             return file_set
 
     def saveFile(self, filename, df):
-        with open(filename, 'w', encoding="utf-8", newline="") as file:
-            pass
+        fnames = ['column1', 'column2', 'column3']
+        data = []
+
+        data1 = {'column1': 'data11', 'column2': 'data12', 'column3': 'data13'}
+        data2 = {'column1': 'data21', 'column2': 'data22', 'column3': 'data23'}
+        data.append(data1)
+        data.append(data2)
+
+        with open(self.root_dir + filename, 'w', encoding="utf-8", newline="") as file:
+            writer = csv.DictWriter(file, fieldnames=fnames)
+            writer.writeheader()
+            for line in data:
+                writer.writerow(line)
+
 
     def loadFile(self, filename):
-        with open(filename, 'r', encoding="utf-8", newline="") as file:
-            pass
+        data = []
+        with open(self.root_dir + filename, 'r', encoding="utf-8", newline="") as file:
+            df = csv.reader(file, delimiter=',', quotechar='|')
+            for row in df:
+                data.append(row)
+
+        return data
--- finance.py
+++ finance.py
@@ -3,6 +3,11 @@ import files
 fm = files.Filemanager()
 encryption = files.Encryption()
 
+fm.saveFile('_2020_february.csv', '22')
 files = fm.listDirectory()
 for f in files:
     print(f)
+
+data = fm.loadFile("_2020_february.csv")
+for row in data:
+    print(row)
